0094d5fb82fad6ef3db8965c65b3b062c5e9f7e6
KyleWren <kyle.wrn.1990@gmail.com>
2017-10-10 14:16:55 -0700

    swift initial commit
    Went over two different versions on how to use a for loop.

---
+++ Swift/week2practice.swift
@@ -0,0 +1,19 @@
+// for loops in swift
+// basic format, counts up to 10
+// for i in 1...10 {
+// 	print( i )
+// }
+
+// let gravity = 9.81
+// var myVariable:Int?
+
+// var start = 1
+// var stop = 10
+// for i in start..<stop {
+// 	print( "displaying i = \(i)" )
+// }
+
+//incrimental increase (every even number)
+for i in stride (from: 0, to: 11, by: 2){
+	print( "current even number \(i)")
+}
