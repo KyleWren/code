451ff478ab7439ce1b1c407b7fdbc565f5caaa5a
KyleWren <kyle.wrn.1990@gmail.com>
2017-10-11 14:10:52 -0700

    repeat while
    next loop

--- Swift/week2practice.swift
+++ Swift/week2practice.swift
@@ -8,3 +8,12 @@ while( increment < 11 )
 	print( "my increment is \(increment)")
 	increment = increment + 1
 }
+
+print( "\n\n\nnow using repeat while\n\n\n")
+increment = 0
+repeat
+// repeat while loop is equivalent to a do-while in (java, C#, C++)
+{
+	print( "my increment is \(increment)")
+	increment = increment + 1
+} while increment < 11
