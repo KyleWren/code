ba67ea5a7e56bd5f23d4670c8bfae09d6c7789cc
KyleWren <kyle.wrn.1990@gmail.com>
2017-10-12 13:24:06 -0700

    swapping variables
    how to swap

--- Swift/week2practice.swift
+++ Swift/week2practice.swift
@@ -1,26 +1,11 @@
-// on the topic of functions
-
-// defined a function "SayHello"
-// - SayHello does not produce an output 
-//(we cannot store its output to a variable)
-
-func SayHello()
-{
-	print( "hello")
-}
-
-// a functioncan take multiple inputs
-// inputs shall not use the var keyword
-// only one output is possible for functions
-func AddIntegers( i1:Int, i2:Int) -> Int
-{
-	let output:Int = i1 + i2
-	return output
-}
-// explicit call to the function SayHello
-SayHello()
-
-// we store the result produced by Addintegers 
-// inside a variable created outside of the function
-var outlet:Int = AddIntegers( i1:1, i2:2)
-print( "\(outlet)")
+var a:Int = 1
+var b:Int = 2
+//swap variables without a=2 b=1
+//a = b (a=2)
+//b = a (b=2)
+var temp = 0
+temp = a
+a = b
+b = temp
+print (a)
+print (b)
---
+++ Swift/work.swift
@@ -0,0 +1,8 @@
+var hourlyRate:Int = 37.5
+var NumHoursWorked:Int = 40
+
+func CalculateGrossIncome()
+{
+	hourlyRate(NumHoursWorked)
+}
+print("value is \(CalculateGrossIncome)")
